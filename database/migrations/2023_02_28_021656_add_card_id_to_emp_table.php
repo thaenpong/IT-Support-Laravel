c46ce8864c5b471442439d45cd74208adf90e1fc
ThaeN <96329582+LunikaX1@users.noreply.github.com>
2023-03-06 09:57:56 +0700

    Update 2023_02_28_021656_add_card_id_to_emp_table.php

--- database/migrations/2023_02_28_021656_add_card_id_to_emp_table.php
+++ database/migrations/2023_02_28_021656_add_card_id_to_emp_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->string('card_id')->after('id');
+            $table->string('card_id')->nullable()->after('id');
         });
     }
 
