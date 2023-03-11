f305727a5e0ccce57903195c8aa7f042d14ca472
ThaeN <thaenpong@gmail.com>
2023-03-11 08:54:33 +0700

    change export excel

--- app/Exports/Registration_Export.php
+++ app/Exports/Registration_Export.php
@@ -3,16 +3,46 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
-
+use App\Models\registration;
+use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Contracts\View\View;
 
 class Registration_Export implements WithMultipleSheets
 {
-
     public function sheets(): array
     {
-        return [
-            new regis_ms_export(),
-            new regis_mt_export(),
-        ];
+        // Get all unique property IDs from the database
+        $propertyIds = registration::distinct()->pluck('property_id');
+
+        $sheets = [];
+
+        // Loop through each property ID and create a new sheet for it
+        foreach ($propertyIds as $propertyId) {
+            $sheets[] = new class($propertyId) implements FromView
+            {
+                private $propertyId;
+
+                public function __construct($propertyId)
+                {
+                    $this->propertyId = $propertyId;
+                }
+
+                public function view(): View
+                {
+                    $data = registration::where('property_id', $this->propertyId)
+                        ->withTrashed()
+                        ->get();
+                    return view('registration.export.regisexport')->with('data', $data);
+                }
+
+                public function title(): string
+                {
+                    // Set the sheet title to the property ID
+                    return 'Property ' . $this->propertyId;
+                }
+            };
+        }
+
+        return $sheets;
     }
 }
